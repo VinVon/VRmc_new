086c698d5cf15a483cb31e074d38fc16b0f7cdb2
VinVon <642751580@qq.com>
2017-11-14 13:43:40 +0800

    心景Vrmc1.0.2 图片资源

---
+++ app/src/main/java/com/medvision/vrmc/bean/CertificationInfo.java
@@ -0,0 +1,202 @@
+package com.medvision.vrmc.bean;
+
+/**
+ * Created by raytine on 2017/8/22.
+ */
+
+public class CertificationInfo {
+
+    /**
+     * token : null
+     * realname : 掌纹升
+     * gender : 1
+     * region : 150100
+     * professionalTitleId : 1
+     * workplaceType : 1
+     * hospital : 医院
+     * department : 可是
+     * position : null
+     * headPictureUrl : http://static.med-vision.cn/test%2F1503371569782.jpg
+     * introduction : null
+     * expertise : null
+     * psychologicalConsultantImageUrl : null
+     * employeeImageUrl : null
+     * doctorProfessionImageUrl : http://static.med-vision.cn/test%2F1503371570687.jpg
+     * professionalQualificationImageUrl : http://static.med-vision.cn/test%2F1503371570901.jpg
+     * status : 4
+     * regionFullName : 内蒙古自治区|呼和浩特市
+     * professionalTitle : 主任医师
+     */
+
+    private Object token;
+    private String realname;
+    private int gender;
+    private String region;
+    private int professionalTitleId;
+    private int workplaceType;
+    private String hospital;
+    private String department;
+    private Object position;
+    private String headPictureUrl;
+    private Object introduction;
+    private Object expertise;
+    private Object psychologicalConsultantImageUrl;
+    private Object employeeImageUrl;
+    private String doctorProfessionImageUrl;
+    private String professionalQualificationImageUrl;
+    private int status;
+    private String regionFullName;
+    private String professionalTitle;
+
+    public Object getToken() {
+        return token;
+    }
+
+    public void setToken(Object token) {
+        this.token = token;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public int getProfessionalTitleId() {
+        return professionalTitleId;
+    }
+
+    public void setProfessionalTitleId(int professionalTitleId) {
+        this.professionalTitleId = professionalTitleId;
+    }
+
+    public int getWorkplaceType() {
+        return workplaceType;
+    }
+
+    public void setWorkplaceType(int workplaceType) {
+        this.workplaceType = workplaceType;
+    }
+
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Object getPosition() {
+        return position;
+    }
+
+    public void setPosition(Object position) {
+        this.position = position;
+    }
+
+    public String getHeadPictureUrl() {
+        return headPictureUrl;
+    }
+
+    public void setHeadPictureUrl(String headPictureUrl) {
+        this.headPictureUrl = headPictureUrl;
+    }
+
+    public Object getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(Object introduction) {
+        this.introduction = introduction;
+    }
+
+    public Object getExpertise() {
+        return expertise;
+    }
+
+    public void setExpertise(Object expertise) {
+        this.expertise = expertise;
+    }
+
+    public Object getPsychologicalConsultantImageUrl() {
+        return psychologicalConsultantImageUrl;
+    }
+
+    public void setPsychologicalConsultantImageUrl(Object psychologicalConsultantImageUrl) {
+        this.psychologicalConsultantImageUrl = psychologicalConsultantImageUrl;
+    }
+
+    public Object getEmployeeImageUrl() {
+        return employeeImageUrl;
+    }
+
+    public void setEmployeeImageUrl(Object employeeImageUrl) {
+        this.employeeImageUrl = employeeImageUrl;
+    }
+
+    public String getDoctorProfessionImageUrl() {
+        return doctorProfessionImageUrl;
+    }
+
+    public void setDoctorProfessionImageUrl(String doctorProfessionImageUrl) {
+        this.doctorProfessionImageUrl = doctorProfessionImageUrl;
+    }
+
+    public String getProfessionalQualificationImageUrl() {
+        return professionalQualificationImageUrl;
+    }
+
+    public void setProfessionalQualificationImageUrl(String professionalQualificationImageUrl) {
+        this.professionalQualificationImageUrl = professionalQualificationImageUrl;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getRegionFullName() {
+        return regionFullName;
+    }
+
+    public void setRegionFullName(String regionFullName) {
+        this.regionFullName = regionFullName;
+    }
+
+    public String getProfessionalTitle() {
+        return professionalTitle;
+    }
+
+    public void setProfessionalTitle(String professionalTitle) {
+        this.professionalTitle = professionalTitle;
+    }
+}
